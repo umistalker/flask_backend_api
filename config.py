a1c2dadebfc1ea03439946ee126043ca40b0f8b6
Mark <umistalker@gmail.com>
2022-01-04 16:20:36 +0300

    changes by deploy on heroku

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import os
 import pathlib
 
 BASE_DIR = pathlib.Path(__file__).parent
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
+
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'you-will-newer-know'
